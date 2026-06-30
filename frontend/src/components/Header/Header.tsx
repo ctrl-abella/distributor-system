6bf578d7c0be2d67f623578dc097988e05b240c3
Franz Christian Abella <abellafranzchristian@gmail.com>
2026-06-30 08:28:08 +0800

    refactor(header): added media query for mobile support

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,30 +1,43 @@
 import styles from "./Header.module.css";
 import logo from "../../assets/images/Lifestyles-Independent-Distributor-Logo.png";
-import { FaHeart, FaShoppingCart } from "react-icons/fa";
+import { FaBars, FaHeart, FaShoppingCart } from "react-icons/fa";
 
-export default function Header(){
-    return (
-        <header className={styles.header}>
-            <div className={styles.headerContainer}>
-                <div className={styles.logoContainer}>
-                    <img src={logo} alt="Independent Distributor Logo" className={styles.logo} />
-                </div>
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <div className={styles.headerContainer}>
+        <button className={styles.menuButton}>
+          <FaBars />
+        </button>
+        <div className={styles.logoContainer}>
+          <img
+            src={logo}
+            alt="Independent Distributor Logo"
+            className={styles.logo}
+          />
+        </div>
 
-                
-                <nav>
-                    <ul className={styles.navList}>
-                        <li><a href="">Home</a></li>
-                        <li><a href="">Shop</a></li>
-                        <li><a href="">About</a></li>
-                        <li><a href="">Contact</a></li>
-                    </ul>
-                </nav>
-                <div className={styles.headerActions}>
-                    <FaHeart className={styles.icon}/>
-                    <FaShoppingCart className={styles.icon}/>
-                </div>
-            </div>
-        </header>
-    )
-    
+        <nav className={styles.nav}>
+          <ul className={styles.navList}>
+            <li className={`${styles.navItem} ${styles.active}`}>
+              <a href="">Home</a>
+            </li>
+            <li className={styles.navItem}>
+              <a href="">Shop</a>
+            </li>
+            <li className={styles.navItem}>
+              <a href="">About</a>
+            </li>
+            <li className={styles.navItem}>
+              <a href="">Contact</a>
+            </li>
+          </ul>
+        </nav>
+        <div className={styles.headerActions}>
+          <FaHeart className={styles.icon} />
+          <FaShoppingCart className={styles.icon} />
+        </div>
+      </div>
+    </header>
+  );
 }
